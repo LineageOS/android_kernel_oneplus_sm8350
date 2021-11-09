f0d22f469c432eeb0a2a3179abe0b55aa3547b93
alk3pInjection <webmaster@raspii.tech>
2021-11-09 14:32:18 +0800

    disp: msm: Implement panel status check
    * to ensure dimlayer has been killed when the panel is off.
    
    Co-authored-by: Art_Chen <Chenxy0201@qq.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>
    Change-Id: I87c861e945eb8728dfd656b20de7ba4fe50b0f09

--- techpack/display/msm/sde/sde_crtc.c
+++ techpack/display/msm/sde/sde_crtc.c
@@ -2866,6 +2866,7 @@ ssize_t notify_fppress_store(struct device *dev,
 
 extern int aod_layer_hide;
 extern int backup_dim_status;
+extern int oneplus_panel_status;
 extern bool backup_dimlayer_hbm;
 extern bool HBM_flag;
 extern bool oneplus_dimlayer_hbm_enable;
@@ -2902,6 +2903,9 @@ ssize_t notify_dim_store(struct device *dev,
 	dsi_connector = dsi_display->drm_conn;
 	mode_config = &drm_dev->mode_config;
 	sscanf(buf, "%du", &dim_status);
+
+	if (oneplus_panel_status == 0)
+		dim_status = 0;
 
 	if(dsi_display->panel->aod_status==0 && (dim_status == 2)){
 		pr_err("fp set it in normal status\n");
