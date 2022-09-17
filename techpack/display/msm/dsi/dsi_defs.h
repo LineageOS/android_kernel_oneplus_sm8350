a697244e6844c9bf763f4f5acade898a8e627cf9
LuK1337 <priv.luk@gmail.com>
2022-09-17 18:12:50 +0200

    Revert "drm: msm: Implement codepath variations for cont-splash DMS"
    This reverts commit 8c3d866c4627340b7f9e08ff0e9a18d267da7cd9.
    
    Change-Id: I45fea9454048da4c4d7ed165d7b1a7580b4b1cdf

--- techpack/display/msm/dsi/dsi_defs.h
+++ techpack/display/msm/dsi/dsi_defs.h
@@ -623,7 +623,6 @@ struct dsi_video_engine_cfg {
 	bool hsa_lp11_en;
 	bool eof_bllp_lp11_en;
 	bool bllp_lp11_en;
-	bool splash_dms;
 	enum dsi_video_traffic_mode traffic_mode;
 	u32 vc_id;
 };
@@ -756,7 +755,6 @@ struct dsi_display_mode {
 #endif
 	enum dsi_op_mode panel_mode;
 	bool is_preferred;
-	bool splash_dms;
 	struct dsi_display_mode_priv_info *priv_info;
 };
 
