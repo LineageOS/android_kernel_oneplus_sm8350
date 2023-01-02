2ad2a744c001418302357327250924c95a891c3e
Alexander Koskovich <akoskovich@pm.me>
2023-01-02 21:36:12 +0800

    drivers: power: oplus: Use CONFIG_QCOM_KGSL for guard instead of CONFIG_DRM_MSM
     * fixes panic due to ui_soc not loading on boot
    
    Change-Id: I285fd7e9c444c247f7e71fd2e96596c3683436c4

--- drivers/power/oplus/oplus_charger.c
+++ drivers/power/oplus/oplus_charger.c
@@ -159,7 +159,7 @@ static void oplus_chg_voter_charging_start(struct oplus_chg_chip *chip,
 extern void oplus_start_svooc_reset(void);
 extern void oplus_start_pps_reset(void);
 static void oplus_chg_check_abnormal_adapter(int vbus_rising);
-#if IS_ENABLED(CONFIG_FB) || IS_ENABLED(CONFIG_DRM_MSM) || IS_ENABLED(CONFIG_DRM_OPLUS_NOTIFY)
+#if IS_ENABLED(CONFIG_FB) || IS_ENABLED(CONFIG_QCOM_KGSL) || IS_ENABLED(CONFIG_DRM_OPLUS_NOTIFY)
 static int fb_notifier_callback(struct notifier_block *nb, unsigned long event, void *data);
 #elif IS_ENABLED(CONFIG_OPLUS_MTK_DRM_GKI_NOTIFY_CHG)
 static int chg_mtk_drm_notifier_callback(struct notifier_block *nb, unsigned long event, void *data);
@@ -3326,7 +3326,7 @@ int oplus_chg_init(struct oplus_chg_chip *chip)
 
 	oplus_pps_init(chip);
 
-#if IS_ENABLED(CONFIG_DRM_MSM) || IS_ENABLED(CONFIG_DRM_OPLUS_NOTIFY)
+#if IS_ENABLED(CONFIG_QCOM_KGSL) || IS_ENABLED(CONFIG_DRM_OPLUS_NOTIFY)
 	chip->chg_fb_notify.notifier_call = fb_notifier_callback;
 	rc = msm_drm_register_client(&chip->chg_fb_notify);
 #elif IS_ENABLED(CONFIG_FB)
