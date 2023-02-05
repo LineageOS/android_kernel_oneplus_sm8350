f51100aeef0c4a9bec0d28430c370a747a739983
xNyaDev <xnyadev@pm.me>
2023-02-05 14:20:28 +0100

    power: oplus_chg_intf: Report discharging status correctly
    Report "Discharging" instead of "Connected, not charging" when there is no
    charger connected.
    
    Change-Id: I2c75cc91d77773c17169d0900995fb07bf95fdd9

--- drivers/power/oplus_chg/oplus_chg_intf.c
+++ drivers/power/oplus_chg/oplus_chg_intf.c
@@ -1420,11 +1420,7 @@ static int oplus_chg_intf_batt_get_prop(struct oplus_chg_mod *ocm,
 			pval->intval = oplus_chg_comm_get_batt_status(oplus_dev->comm_ocm);
 		} else {
 			if (!usb_online && !wls_online) {
-#ifdef 	CONFIG_OPLUS_CHG_OOS
 				pval->intval = POWER_SUPPLY_STATUS_DISCHARGING;
-#else
-				pval->intval = POWER_SUPPLY_STATUS_NOT_CHARGING;
-#endif
 			} else if ((oplus_chg_show_warp_logo_ornot() == 1) ||
 				   oplus_warp_ignore_event() ||
 				   oplus_dev->icon_debounce) {
