cb1a0e270e99734d3ce64679ad2e730a4dbdf77a
pjgowtham <pjgowtham@gmail.com>
2023-02-19 18:30:17 +0530

    icnss2: Import additional changes and enable BDF2 feature
    This fixes the issue where 5ghz wifi would not work properly on realme devices
    
    Change-Id: Iccafda605793622d81c23dd8ff5843388a731e68

--- drivers/soc/qcom/icnss2/main.c
+++ drivers/soc/qcom/icnss2/main.c
@@ -3907,8 +3907,12 @@ void icnss_add_fw_prefix_name(struct icnss_priv *priv, char *prefix_name,
 		return;
 	}
 
+#if IS_ENABLED(CONFIG_OPLUS_FEATURE_WIFI_BDF2)
+	scnprintf(prefix_name, ICNSS_MAX_FILE_NAME, "%s", name);
+#else
 	scnprintf(prefix_name, ICNSS_MAX_FILE_NAME,
 		  QCA6750_PATH_PREFIX "%s", name);
+#endif /* CONFIG_OPLUS_FEATURE_WIFI_BDF2 */
 
 	icnss_pr_dbg("File added with prefix: %s\n", prefix_name);
 }
