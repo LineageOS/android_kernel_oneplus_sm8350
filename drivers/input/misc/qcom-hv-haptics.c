478a11ba02199f87e0c58d7b43430fb18cc243ea
pjgowtham <pjgowtham@gmail.com>
2023-05-20 13:04:58 +0530

    qcom-hv-haptics: Read fifo-vmax-mv from device tree for custom effects
    oplus defines fifo-vmax-mv in device tree to use different vmax for constant effects vs fifo effects.
    
    Change-Id: I8f13e8039ccccc2ee3d8f506087eb263651effed

--- drivers/input/misc/qcom-hv-haptics.c
+++ drivers/input/misc/qcom-hv-haptics.c
@@ -28,6 +28,9 @@
 #include <linux/uaccess.h>
 #include <linux/qpnp/qpnp-pbs.h>
 
+#undef dev_dbg
+#define dev_dbg dev_err
+
 /* status register definitions in HAPTICS_CFG module */
 #define HAP_CFG_REVISION2_REG			0x01
 #define HAP_CFG_V1				0x1
@@ -479,6 +482,7 @@ struct haptics_play_info {
 struct haptics_hw_config {
 	struct brake_cfg	brake;
 	u32			vmax_mv;
+	u32			fifo_vmax_mv;
 	u32			t_lra_us;
 	u32			cl_t_lra_us;
 	u32			lra_min_mohms;
@@ -2167,7 +2171,7 @@ static int haptics_init_custom_effect(struct haptics_chip *chip)
 	chip->custom_effect->pattern = NULL;
 	chip->custom_effect->brake = NULL;
 	chip->custom_effect->id = UINT_MAX;
-	chip->custom_effect->vmax_mv = chip->config.vmax_mv;
+	chip->custom_effect->vmax_mv = chip->config.fifo_vmax_mv;
 	chip->custom_effect->t_lra_us = chip->config.t_lra_us;
 	chip->custom_effect->src = FIFO;
 	chip->custom_effect->auto_res_disable = true;
@@ -4087,6 +4091,11 @@ static int haptics_parse_dt(struct haptics_chip *chip)
 				config->vmax_mv, MAX_VMAX_MV);
 		rc = -EINVAL;
 		goto free_pbs;
+	}
+
+	rc = of_property_read_u32(node, "qcom,fifo-vmax-mv", &config->fifo_vmax_mv);
+	if (rc || config->fifo_vmax_mv >= MAX_VMAX_MV) {
+		config->fifo_vmax_mv = DEFAULT_VMAX_MV;
 	}
 
 	config->fifo_empty_thresh = get_fifo_empty_threshold(chip);
