e497bde953f9592794f80beebcc5e9abbd2088a3
Rahul Choudhary <quic_rahulc@quicinc.com>
2023-07-10 07:16:08 -0700

    Release 2.0.8.33L
    Release 2.0.8.33L
    
    Change-Id: Id6926128d04ec244a8f0df421715ea2207bcb44c
    CRs-Fixed: 774533

--- core/mac/inc/qwlan_version.h
+++ core/mac/inc/qwlan_version.h
@@ -32,9 +32,9 @@
 #define QWLAN_VERSION_MAJOR            2
 #define QWLAN_VERSION_MINOR            0
 #define QWLAN_VERSION_PATCH            8
-#define QWLAN_VERSION_EXTRA            "K"
+#define QWLAN_VERSION_EXTRA            "L"
 #define QWLAN_VERSION_BUILD            33
 
-#define QWLAN_VERSIONSTR               "2.0.8.33K"
+#define QWLAN_VERSIONSTR               "2.0.8.33L"
 
 #endif /* QWLAN_VERSION_H */
