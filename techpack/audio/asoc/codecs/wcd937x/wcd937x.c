f286b6fbf38315615a7346fae5237f4364fdf146
Alexander Winkowski <dereference23@outlook.com>
2023-07-11 09:00:48 +0000

    techpack: audio: Resolve duplicate symbols
    They're used only in their own source files so just mark them static.
    
    Change-Id: Ie8699f97074a5838af4bc6eab4c1108258b97e4c
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- techpack/audio/asoc/codecs/wcd937x/wcd937x.c
+++ techpack/audio/asoc/codecs/wcd937x/wcd937x.c
@@ -2072,7 +2072,7 @@ static const char * const rx_hph_mode_mux_text[] = {
 	"CLS_H_ULP", "CLS_AB_HIFI",
 };
 
-const char * const tx_master_ch_text[] = {
+static const char * const tx_master_ch_text[] = {
 	"ZERO", "SWRM_TX1_CH1", "SWRM_TX1_CH2", "SWRM_TX1_CH3", "SWRM_TX1_CH4",
 	"SWRM_TX2_CH1", "SWRM_TX2_CH2", "SWRM_TX2_CH3", "SWRM_TX2_CH4",
 	"SWRM_TX3_CH1", "SWRM_TX3_CH2", "SWRM_TX3_CH3", "SWRM_TX3_CH4",
@@ -2080,7 +2080,7 @@ const char * const tx_master_ch_text[] = {
 	"DMIC4", "DMIC5", "DMIC6", "DMIC7",
 };
 
-const struct soc_enum tx_master_ch_enum =
+static const struct soc_enum tx_master_ch_enum =
 	SOC_ENUM_SINGLE_EXT(ARRAY_SIZE(tx_master_ch_text),
 					tx_master_ch_text);
 
--- techpack/audio/asoc/codecs/wcd938x/wcd938x.c
+++ techpack/audio/asoc/codecs/wcd938x/wcd938x.c
@@ -2791,14 +2791,14 @@ static int wcd938x_ldoh_put(struct snd_kcontrol *kcontrol,
 	return 0;
 }
 
-const char * const tx_master_ch_text[] = {
+static const char * const tx_master_ch_text[] = {
 	"ZERO", "SWRM_TX1_CH1", "SWRM_TX1_CH2", "SWRM_TX1_CH3", "SWRM_TX1_CH4",
 	"SWRM_TX2_CH1", "SWRM_TX2_CH2", "SWRM_TX2_CH3", "SWRM_TX2_CH4",
 	"SWRM_TX3_CH1", "SWRM_TX3_CH2", "SWRM_TX3_CH3", "SWRM_TX3_CH4",
 	"SWRM_PCM_IN",
 };
 
-const struct soc_enum tx_master_ch_enum =
+static const struct soc_enum tx_master_ch_enum =
 	SOC_ENUM_SINGLE_EXT(ARRAY_SIZE(tx_master_ch_text),
 					tx_master_ch_text);
 
